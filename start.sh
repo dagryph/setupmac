ed5871789b773ed1aff5602e627efdbabee54ef0
Jeffrey Bernt <dagryph@users.noreply.github.com>
2017-05-01 20:49:32 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -31,7 +31,7 @@ sudo easy_install ansible
 installdir="/tmp/setupmac-$RANDOM"
 mkdir $installdir
 
-git clone https://github.com/daemonza/setupmac.git $installdir 
+git clone https://github.com/dagryph/setupmac.git $installdir 
 if [ ! -d $installdir ]; then
     echo "failed to find setupmac."
     echo "git cloned failed"
